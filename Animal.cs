88b5b6b921e2f0cc5bf4ee770a4f4f094ee173cc
Daniel Hultmark <danielhultmark@gmail.com>
2025-09-25 14:54:10 +0200

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPArv
+{
+    internal class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} - {Age} years old is eating!");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine($"{Name} is sleeping!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OOPArv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat();
+            myCat.Name = "Misse";
+            myCat.Age = 3;
+            myCat.Eat();
+            myCat.Meow();
+
+            Dog myDog = new Dog();
+            myDog.Name = "Fido";
+            myDog.Age = 5;
+            myDog.Eat();
+
+            
+        }
+    }
+}
